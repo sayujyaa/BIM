61124e4d05bceb46e1b3778bbc6af79285223658
Sayujya <sayujya57@gmail.com>
2026-05-31 15:22:08 +0545

    Add upto Lab 3 BI

---
+++ 08_BIM8th/BI/InsuranceReports/queries/SqlQuery_Report.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+    d.MonthName,
+    d.YearValue,
+    c.CategoryName,
+    f.PoliciesCount,
+    f.TotalPremium,
+    f.SumInsured
+FROM FactInsuranceBusiness f
+INNER JOIN DimDate d
+    ON f.DateID = d.DateID
+INNER JOIN DimInsuranceCategory c
+    ON f.CategoryID = c.CategoryID;
